275b2c72d9c116e7681f4e2f665c7eb6bf8b23d8
ethan-heo <ethan.hb94@gmail.com>
2024-07-16 23:21:32 +0900

    chore: 초기 개발 설정

---
+++ apps/fetching-large-size-data-in-nextjs/app/WrappedFullcalendar.tsx
@@ -0,0 +1,10 @@
+import FullCalendar from '@fullcalendar/react'
+import React from 'react'
+
+function WrappedFullcalendar() {
+  return (
+    <FullCalendar />
+  )
+}
+
+export default WrappedFullcalendar
