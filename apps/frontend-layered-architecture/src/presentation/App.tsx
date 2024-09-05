d1fd9629180a4682187ea4bb7583aa7b41c6b6fb
ethan-heo <ethan.hb94@gmail.com>
2024-09-05 22:29:20 +0900

    feat: Todo UI 구현을 위한 설정작업을 진행한다

--- apps/frontend-layered-architecture/src/presentation/App.tsx
+++ apps/frontend-layered-architecture/src/presentation/App.tsx
@@ -1,50 +1,13 @@
-import { useEffect, useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { AppState, TodoApp } from "../application";
+import { Paragraph, Title } from "@ethanheo/ui";
+import Todo from "./Todo";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const todo = useSelector<AppState, TodoApp.TodoState>((state) => state.todo);
-  const dispatch = useDispatch();
-
-  console.log(todo);
-
-  useEffect(() => {
-    dispatch(
-      TodoApp.addTodoAction({
-        content: "hello world",
-        status: "NOT_READY",
-        importance: false,
-      }),
-    );
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <Title>TodoList Example</Title>
+      <Paragraph>- Frontend Layered Architecture</Paragraph>
+      <Todo />
+    </main>
   );
 }
 
--- apps/frontend-layered-architecture/src/presentation/main.tsx
+++ apps/frontend-layered-architecture/src/presentation/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { Provider } from "react-redux";
 import createStore from "../application/createStore.ts";
+import "./reset.css";
+import "@ethanheo/ui/styles/light-theme.css";
+import "@ethanheo/ui/styles/components.css";
 
 const store = createStore();
 
